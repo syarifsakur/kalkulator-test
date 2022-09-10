ea91176080e06eb1a6f48fd7ed9b5e698b856847
syarifsakur <107300773+syarifsakur@users.noreply.github.com>
2022-09-10 17:17:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
-package kalkulator
+package Kalkulator
 
 import(
 	"fmt"
 )
-func tambah(a,b int) int{
+func Tambah(a,b int) int{
 	return a + b
 }
-func kurang(a,b int) int{
+func Kurang(a,b int) int{
 	return a - b
 }
-func kali(a,b int) int{
+func Kali(a,b int) int{
 	return a * b
 }
-func bagi(a,b int) int{
+func Bagi(a,b float64) float64{
 	return a / b
 }
 
 func main(){
-	result1 := tambah(5,3) 
-	result2 := kurang(5,3) 
-	result3 := kali(5,3) 
-	result4 := bagi(5,3) 
+	result1 := Tambah(5,3) 
+	result2 := Kurang(5,3) 
+	result3 := Kali(5,3) 
+	result4 := Bagi(5,3) 
 
 	fmt.Println(result1)
 	fmt.Println(result2)
